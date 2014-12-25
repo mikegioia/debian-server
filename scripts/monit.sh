708aaf5fc2d6c1a10bab1f7967f469bf9a0524c2
Mike Gioia <mikegioia@gmail.com>
2014-12-25 11:38:40 -0500

    Set up monit script

--- scripts/monit.sh
+++ scripts/monit.sh
@@ -3,25 +3,39 @@
 # Installs Monit and copies over config file
 ##
 
-echo 'This script will install Monit from source.'
-read -p 'Do you want to continue [y/N]? ' wish
-if ! [[ "$wish" == "y" || "$wish" == "Y" ]] ; then
-    echo "Aborted"
-    exit
-fi
+## Prompt to continue
+function promptInstall {
+    echo -e "\n${blueBgWhiteBold}This script will install monit.${NC}"
+    read -p 'Do you want to continue [y/N]? ' wish
+    if ! [[ "$wish" == "y" || "$wish" == "Y" ]] ; then
+        exit 0
+    fi
+}
 
 ## Install if it isn't already
-if ! [[ `hash monit 2>/dev/null` ]] ; then
-    apt-get install monit
-    update-rc.d monit defaults
-fi
+function installMonit {
+    if ! [[ `hash monit 2>/dev/null` ]] ; then
+        echo -e "${green}Installing monit and adding monit to system startup${NC}"
+        apt-get install monit
+        update-rc.d monit defaults
+    else
+        echo -e "${yellow}Monit already installed${NC}"
+        read -p 'Do you want to add monit to system startup [y/N] ' wish
+        if [[ "$wish" == "y" || "$wish" == "Y" ]] ; then
+            update-rc.d monit defaults
+        fi
+    fi
+}
 
 ## Copy over the monitrc file and reload monit
-if [[ -f "$basepath/conf/$profile/monitrc" ]] ; then
-    cp $basepath/conf/$profile/monitrc /etc/monit/monitrc
-fi
+function copyRcFile {
+    if [[ -f "$basepath/conf/$profile/monitrc" ]] ; then
+        cp $basepath/conf/$profile/monitrc /etc/monit/monitrc
+    fi
+    monit reload
+}
 
-monit reload
-
-echo 'Monit completed'
-echo ''
+promptInstall
+installMonit
+copyRcFile
+exit 0
