8fc0a33235d28b01ca51e4cc167ecac0aaefbb4a
Mike Gioia <mikegioia@gmail.com>
2014-12-26 15:26:21 -0500

    Set up xtrabackup script

--- scripts/xtrabackup.sh
+++ scripts/xtrabackup.sh
@@ -3,24 +3,40 @@
 # Install and configure Percona XtraBackup
 ##
 
-echo 'This script will install Percona XtraBackup'
-read -p 'Do you want to continue [y/N]? ' wish
-if ! [[ "$wish" == "y" || "$wish" == "Y" ]] ; then
-    echo "Aborted"
-    exit 0
-fi
+## Prompt to continue
+function promptInstall {
+    echo -e "\n${blueBgWhiteBold}This script will install Percona XtraBackup.${NC}"
+    read -p 'Do you want to continue [y/N]? ' wish
+    if ! [[ "$wish" == "y" || "$wish" == "Y" ]] ; then
+        exit 0
+    fi
+}
 
-## Add apt list and update
-if ! [[ -f "/etc/apt/sources.list.d/xtrabackup.list" ]] ; then
-    echo '  --> adding XtraBackup source and fetching key'
-    echo 'deb http://repo.percona.com/apt wheezy main' > /etc/apt/sources.list.d/xtrabackup.list
-    echo 'deb-src http://repo.percona.com/apt wheezy main' >> /etc/apt/sources.list.d/xtrabackup.list
-    apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
-    apt-get update
-fi
+## Echo this out to xtrabackup.list if file not found
+function addAptSource {
+    if ! [[ -f "/etc/apt/sources.list.d/xtrabackup.list" ]] ; then
+        echo -e "${green}Adding XtraBackup source and fetching key${NC}"
+        echo 'deb http://repo.percona.com/apt wheezy main' > /etc/apt/sources.list.d/xtrabackup.list
+        echo 'deb-src http://repo.percona.com/apt wheezy main' >> /etc/apt/sources.list.d/xtrabackup.list
+        apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+        apt-get update
+    else
+        echo -e "${yellow}Skipping, XtraBackup source set in /etc/apt/sources.list.d/${NC}"
+    fi
+}
 
-echo '  --> installing XtraBackup'
-apt-get install xtrabackup
+## Install xtrabackup
+function installXtrabackup {
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' xtrabackup|grep "install ok installed")
+    if [[ "" == "$PKG_OK" ]] ; then
+        echo -e "${green}Installing XtraBackup from apt${NC}"
+        apt-get install xtrabackup
+    else
+        echo -e "${yellow}XtraBackup already installed${NC}"
+    fi
+}
 
-echo 'XtraBackup completed'
-echo ''
+promptInstall
+addAptSource
+installXtrabackup
+exit 0
